a84cd87b7a49135da67b09ccfe40ca7c964a1029
slplaine <simoneleca@gmail.com>
2026-02-10 09:22:34 -0300

    making improvements

---
+++ week05/bom.js
@@ -0,0 +1,38 @@
+// Referências aos elementos principais
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#button');
+const list = document.querySelector('#list');
+let chaptersArray = getChapterList() || [];
+
+// Evento de clique no botão
+button.addEventListener('click', function() {
+  // Verifica se o campo não está vazio
+  if (input.value.trim() !== '') {
+    // Cria o item da lista
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    // Define o texto do item e do botão
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+    // Adiciona o botão dentro do li
+    li.append(deleteButton);
+
+    // Adiciona o li dentro da lista
+    list.append(li);
+
+    // Evento de clique no botão de deletar
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    // Limpa o campo de entrada
+    input.value = '';
+  }
+
+  // Sempre retorna o foco para o input
+  input.focus();
+});
