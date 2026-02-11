0699b3bfe005164323a0745d1a29634c3d4dba3f
slplaine <simoneleca@gmail.com>
2026-02-10 22:01:19 -0300

    making improvements

--- week05/bom.js
+++ week05/bom.js
@@ -28,6 +28,7 @@ function displayList(item) {
     deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
     input.focus(); // set the focus back to the input
   });
+  console.log('Now I understand the code 😎');
 }
 function setChapterList() {
   localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
